80e6478b9f158aabb040fda63332989bf6e45e29
ThalhaAnas <thalhaanastwo@gmail.com>
2025-11-20 10:51:10 +0530

    Participant class initiate

---
+++ src/Participant.java
@@ -0,0 +1,11 @@
+public class Participant {
+
+    private String id;
+    private String name;
+    private String email;
+    private String preferredGame;
+    private int skillLevel;
+    private String preferredRole;
+    private int personalityScore;
+    private String personalityType;
+}
