4b271946ce53b338d895ab98eb32bd71bcd7f7cf
ThalhaAnas <thalhaanastwo@gmail.com>
2025-11-23 20:13:00 +0530

    team participants details

--- src/Team.java
+++ src/Team.java
@@ -1,16 +1,16 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Team{
+public class Team {
     private String teamID;
     private List<Participant> members;
 
-    public Team(){
+    public Team() {
         this.members = new ArrayList<>();
     }
 
     //constructors
-    public Team(String teamID){
+    public Team(String teamID) {
         this.teamID = teamID;
         this.members = new ArrayList<>();
     }
@@ -19,6 +19,7 @@ public class Team{
     public String getTeamID() {
         return teamID;
     }
+
     public void setTeamID(String teamID) {
         this.teamID = teamID;
     }
@@ -30,30 +31,52 @@ public class Team{
     public void setMembers(List<Participant> members) {
         this.members = members;
     }
+
     // methods used to manage teams
-    public void addMember(Participant participant){
-        if (participant != null){
+    public void addMember(Participant participant) {
+        if (participant != null) {
             members.add(participant);
         }
     }
 
-    public void removeMember(Participant participant){
+    public void removeMember(Participant participant) {
         members.remove(participant);
     }
 
-    public int getTeamSize(){
+    public int getTeamSize() {
         return members.size();
     }
 
-    public double getAverageSkill(){
-        if (members.isEmpty()){
+    public double getAverageSkill() {
+        if (members.isEmpty()) {
             return 0;
         }
 
         int totalSkill = 0;
-        for (Participant member : members){
+        for (Participant member : members) {
             totalSkill += member.getSkillLevel();
         }
-        return (double)totalSkill/members.size();
+        return (double) totalSkill / members.size();
+    }
+
+    @Override
+    public String toString() {
+        return "Team [teamID=" + teamID + ", size=" + getTeamSize() + ", avgSkill=" + getAverageSkill() + "]";
+    }
+
+    public String getTeamDetails() {
+        StringBuilder details = new StringBuilder();
+        details.append("Team: ").append(teamID)
+                .append(" | Size: ").append(getTeamSize())
+                .append(" | AverageSkill: ").append(getAverageSkill())
+                .append("\nMembers: ");
+
+        for (Participant member : members) {
+            details.append(" - ").append(member.getName())
+                    .append(" (").append(member.getPreferredRole())
+                    .append(", ").append(member.getPersonalityType())
+                    .append(")\n");
+        }
+        return details.toString();
     }
 }
